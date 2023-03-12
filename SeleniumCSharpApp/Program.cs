26ca254a344bb6e52ca0c63acbdc5a72135fa774
aishahfaisal <s.aishahfaisal@gmail.com>
2023-03-13 01:12:30 +0800

    first commit

--- SeleniumCSharpApp/Program.cs
+++ SeleniumCSharpApp/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SeleniumCSharpApp;
 
+var MySeleniumTest = new MySeleniumTest();
+MySeleniumTest.Test1();
---
+++ SeleniumCSharpApp/SeleniumCSharpApp.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SeleniumCSharpApp
+
+{
+    public class MySeleniumTest
+    {
+        [Test]
+        public void Test1()
+        {
+            //No GUI - reduce time. Remove 'options' from code line 18 to run GUI.
+            ChromeOptions options = new();
+            options.AddArgument("--headless");
+
+            //Create a new instance of the Chrome driver
+            IWebDriver driver = new ChromeDriver(options);
+
+            Console.WriteLine("Start testing... Please wait..");
+
+            //Navigate to Amazon
+            driver.Navigate().GoToUrl("https://www.amazon.com");
+            Assert.That(driver.Title, Is.EqualTo("Amazon.com. Spend less. Smile more."));
+
+            //Find the search text box and type in "laptop"
+            IWebElement searchBox = driver.FindElement(By.Id("twotabsearchtextbox"));
+            searchBox.SendKeys("laptop");
+
+            //Find the search button and click it 
+            IWebElement searchButton = driver.FindElement(By.Id("nav-search-submit-button"));
+            searchButton.Click();
+
+            //Find the search results element and verify it contains results
+            IWebElement searchResults = driver.FindElement(By.ClassName("s-result-list"));
+            Assert.IsTrue(searchResults.Displayed, "Search results are not displayed");
+
+            //Verify correct result = 'laptop'
+            IList<IWebElement> laptopLinks = searchResults.FindElements(By.CssSelector("a[href*='laptop']"));
+            Assert.That(laptopLinks.Count, Is.GreaterThan(0), "No laptop products in search results");
+
+            //Find the first search result title link and click it
+            IWebElement firstResult = driver.FindElement(By.CssSelector("div[data-component-type='s-search-result'] h2 a"));
+            firstResult.Click();
+
+            //Wait for the page to load
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //Get the price element
+            IWebElement priceElement = driver.FindElement(By.CssSelector(".a-price-whole"));
+
+            //Assign the price element to string variable
+            string priceString = priceElement.Text;
+            decimal price = decimal.Parse(priceString);
+
+            //Assert that the laptop price is more than $100
+            Assert.That(price, Is.GreaterThan(100), "Expected price is more than $100");
+
+            //Close the browser
+            driver.Quit();
+
+            Console.WriteLine("Test has finished run.. Thanks!");
+        }
+    }
+}
